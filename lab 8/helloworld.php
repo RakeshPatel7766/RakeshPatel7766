0850b74cc7bd741fc7add8a8dfbd2696863850f8
RakeshPatel7766 <987024@win.edu.au>
2024-09-12 16:24:03 +1000

    Add files via upload

---
+++ lab 8/helloworld.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title> first php code </title>
+</head>
+<body>
+<h1>
+<?php
+$counter = 10;
+/* while condition */
+while($counter>0)
+{
+// print counter
+echo(" Counter is $counter <br>");
+// reduce the counter by 1
+$counter=$counter-1;
+}
+?>
+</h1>
+</body>
+</html>
---
+++ lab 8/root.php
@@ -0,0 +1,29 @@
+<HTML>
+<head>
+<title> Get a single record from a table example code </title>
+</head>
+<BODY>
+	<TABLE>
+		<TH>Number</TH><TH>Last Name</TH><TH>First Name</TH>
+		<?php 
+			// $conn = mysqli_connect(“host”,”user”,”password”,"Database");
+			$conn = mysqli_connect("localhost","root","","saleco");
+			// Check connection
+				if (!$conn) { die("Connection failed: " . mysqli_connect_error());}
+				echo "Connected successfully <br>";
+			// once conenction established prepare a query
+			$query = "Select * FROM tblcustomer where cuscode=10010 ";
+			// Run the query
+			$result = mysqli_query($conn,$query) or die("Unable to run the query");
+			// Notice the use of mysqli_num_rows
+			echo("<br> Select returned  ". mysqli_num_rows($result)."  rows. <br><br>");
+			// Print the result line by line	
+			while ($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+				echo "<TR><TD>$row[0]</TD><TD>$row[1]</TD><TD>$row[2]</TD></TR>";
+			}//End of while
+			//Close conenction to the database
+			mysqli_close($conn);
+		?>
+	</TABLE>
+</BODY>
+</HTML>
